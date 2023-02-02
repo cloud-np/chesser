cd282b6add90a80a083632d34a37b029a993fd82
Nick Pappas <nick.pappas@solera.com>
2023-02-02 10:20:02 +0100

    Adding authetication

--- api/server.js
+++ api/server.js
@@ -5,15 +5,19 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env.example' });
 const helmet = require('helmet');
 const morgan = require('morgan');
+const postRoutes = require('./routes/postRoutes');
 
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+
 // Security
-// app.use(helmet());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
 // Logging
 app.use(morgan("common"));
+app.use("/api/posts", postRoutes);
 
 const posts = [
   {
@@ -26,10 +30,6 @@ const posts = [
   }
 ]
 
-app.get('/posts', authenticateToken, (req, res) => {
-//   res.json(posts.filter(post => post.username === req.user.name))
-  res.json('eeeeeeeeeeeeelaaa')
-})
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
@@ -44,15 +44,25 @@ function authenticateToken(req, res, next) {
   })
 }
 
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    console.log(`${username} is trying to login ..`);
+app.get('/test', authenticateToken, (req, res) => {
+  res.status(200).json({ message: 'success!' })
+});
 
-    return res.json({ token: jwt.sign(username, process.env.JWT_ACCESS_TOKEN) });
+app.post("/login", (req, res) => {
+  // if (!req.headers['content-length']) {
+  //   return res.status(400).json({ message: 'Empty request body' });
+  // }
+  // const contentLength = parseInt(req.headers['content-length'], 10);
+  // if (contentLength === 0) {
+  //   return res.status(400).json({ message: 'Empty request body' });
+  // }
+  // console.log(`${username} is trying to login ..`);
+
+  return res.status(200).json({ token: jwt.sign('test-username', process.env.JWT_ACCESS_TOKEN) });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
